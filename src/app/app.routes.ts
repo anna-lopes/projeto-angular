c594794b9bcadb18a238fec11075d562d6de8b3d
anna-lopes <161318423+anna-lopes@users.noreply.github.com>
2024-11-04 08:58:40 -0300

    Add files via upload

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: HomeComponent },  // Rota para a página inicial
+  { path: 'about', component: AboutComponent },  // Rota para a página "About"
+  { path: 'contact', component: ContactComponent }  // Rota para a página "Contact"
+];
